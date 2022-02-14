ed9a4053828449b4fa112a6b43928ee3bcc3f685
RobertJacksonIO <jackson.t.robert@gmail.com>
2022-02-14 08:54:35 -0700

    test[pkg/alienmap/map.go]: Adding testing.

--- pkg/alienmap/map_test.go
+++ pkg/alienmap/map_test.go
@@ -22,19 +22,43 @@ func TestRemoveCity(t *testing.T) {
 	assert.NoError(t, err)
 	err = m.RemoveCity("Baz")
 	assert.NoError(t, err)
+	_, exists := m.cities["Baz"]
+	assert.Equal(t, exists, false)
 }
+
+func TestAddCity(t *testing.T) {
+	m, err := helper()
+	assert.NoError(t, err)
+	newCity := City{"Paris"}
+	err = m.AddCity(&newCity)
+	assert.NoError(t, err)
+	_, exists := m.cities["Paris"]
+	assert.Equal(t, exists, true)
+}
+
+func TestHasNeighbors(t *testing.T) {
+	m, err := helper()
+	assert.NoError(t, err)
+	city := m.cities["Foo"]
+	assert.Equal(t, m.hasNeighbors(city), true)
+}
+
+func TestMakeAliens(t *testing.T) {
+	assert.Equal(t, len(makeAliens(1, 1000)), 1000)
+}
+
 func TestFirstWave(t *testing.T) {
 	m, err := helper()
 	assert.NoError(t, err)
-	m.FirstWave(5)
+	err = m.FirstWave(5)
+	assert.NoError(t, err)
 }
 
 func TestSimulate(t *testing.T) {
 	m, err := helper()
 	assert.NoError(t, err)
-	m.PrintMap()
-	m.Simulate(5, 10)
-	m.PrintMap()
+	err = m.Simulate(5, 10)
+	assert.NoError(t, err)
 }
 
 func helper() (Map, error) {
